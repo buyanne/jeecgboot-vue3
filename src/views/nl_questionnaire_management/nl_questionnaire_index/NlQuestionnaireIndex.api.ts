7ba2aadf2cd9a0c5532bbe891ae216c90d33a321
buyanne <88044787+buyanne@users.noreply.github.com>
2024-04-20 10:05:18 +0800

    指标管理的树下拉，以及题库管理初始化

--- src/views/nl_questionnaire_management/nl_questionnaire_index/NlQuestionnaireIndex.api.ts
+++ src/views/nl_questionnaire_management/nl_questionnaire_index/NlQuestionnaireIndex.api.ts
@@ -1,17 +1,19 @@
-import {defHttp} from '/@/utils/http/axios';
+import { defHttp } from "/@/utils/http/axios";
 import { useMessage } from "/@/hooks/web/useMessage";
 
 const { createConfirm } = useMessage();
 
 enum Api {
-  list = '/nl_questionnaire_index/nlQuestionnaireIndex/list',
-  save='/nl_questionnaire_index/nlQuestionnaireIndex/add',
-  edit='/nl_questionnaire_index/nlQuestionnaireIndex/edit',
-  deleteOne = '/nl_questionnaire_index/nlQuestionnaireIndex/delete',
-  deleteBatch = '/nl_questionnaire_index/nlQuestionnaireIndex/deleteBatch',
-  importExcel = '/nl_questionnaire_index/nlQuestionnaireIndex/importExcel',
-  exportXls = '/nl_questionnaire_index/nlQuestionnaireIndex/exportXls',
+  list = "/nl_questionnaire_index/nlQuestionnaireIndex/list",
+  save = "/nl_questionnaire_index/nlQuestionnaireIndex/add",
+  edit = "/nl_questionnaire_index/nlQuestionnaireIndex/edit",
+  deleteOne = "/nl_questionnaire_index/nlQuestionnaireIndex/delete",
+  deleteBatch = "/nl_questionnaire_index/nlQuestionnaireIndex/deleteBatch",
+  importExcel = "/nl_questionnaire_index/nlQuestionnaireIndex/importExcel",
+  exportXls = "/nl_questionnaire_index/nlQuestionnaireIndex/exportXls",
+  getIndexTree="/nl_questionnaire_index/nlQuestionnaireIndex/getIndexTree"
 }
+
 /**
  * 导出api
  * @param params
@@ -26,39 +28,43 @@ export const getImportUrl = Api.importExcel;
  * @param params
  */
 export const list = (params) =>
-  defHttp.get({url: Api.list, params});
-
+  defHttp.get({ url: Api.list, params });
+export const getIndexTree = (params) =>
+  defHttp.get({ url: Api.getIndexTree, params });
 /**
  * 删除单个
  */
-export const deleteOne = (params,handleSuccess) => {
-  return defHttp.delete({url: Api.deleteOne, params}, {joinParamsToUrl: true}).then(() => {
+export const deleteOne = (params, handleSuccess) => {
+  return defHttp.delete({ url: Api.deleteOne, params }, { joinParamsToUrl: true }).then(() => {
     handleSuccess();
   });
-}
+};
 /**
  * 批量删除
  * @param params
  */
 export const batchDelete = (params, handleSuccess) => {
   createConfirm({
-    iconType: 'warning',
-    title: '确认删除',
-    content: '是否删除选中数据',
-    okText: '确认',
-    cancelText: '取消',
+    iconType: "warning",
+    title: "确认删除",
+    content: "是否删除选中数据",
+    okText: "确认",
+    cancelText: "取消",
     onOk: () => {
-      return defHttp.delete({url: Api.deleteBatch, data: params}, {joinParamsToUrl: true}).then(() => {
+      return defHttp.delete({
+        url: Api.deleteBatch,
+        data: params
+      }, { joinParamsToUrl: true }).then(() => {
         handleSuccess();
       });
     }
   });
-}
+};
 /**
  * 保存或者更新
  * @param params
  */
 export const saveOrUpdate = (params, isUpdate) => {
   let url = isUpdate ? Api.edit : Api.save;
-  return defHttp.post({url: url, params});
-}
+  return defHttp.post({ url: url, params });
+};
