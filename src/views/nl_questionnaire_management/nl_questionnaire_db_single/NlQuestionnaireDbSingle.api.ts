cf9db15cd7c2e150ea55cb088c01b7b135b688da
buyanne <88044787+buyanne@users.noreply.github.com>
2024-04-20 10:42:01 +0800

    初始化题库管理

---
+++ src/views/nl_questionnaire_management/nl_questionnaire_db_single/NlQuestionnaireDbSingle.api.ts
@@ -0,0 +1,64 @@
+import {defHttp} from '/@/utils/http/axios';
+import { useMessage } from "/@/hooks/web/useMessage";
+
+const { createConfirm } = useMessage();
+
+enum Api {
+  list = '/nl_questionnaire_db_single/nlQuestionnaireDbSingle/list',
+  save='/nl_questionnaire_db_single/nlQuestionnaireDbSingle/add',
+  edit='/nl_questionnaire_db_single/nlQuestionnaireDbSingle/edit',
+  deleteOne = '/nl_questionnaire_db_single/nlQuestionnaireDbSingle/delete',
+  deleteBatch = '/nl_questionnaire_db_single/nlQuestionnaireDbSingle/deleteBatch',
+  importExcel = '/nl_questionnaire_db_single/nlQuestionnaireDbSingle/importExcel',
+  exportXls = '/nl_questionnaire_db_single/nlQuestionnaireDbSingle/exportXls',
+}
+/**
+ * 导出api
+ * @param params
+ */
+export const getExportUrl = Api.exportXls;
+/**
+ * 导入api
+ */
+export const getImportUrl = Api.importExcel;
+/**
+ * 列表接口
+ * @param params
+ */
+export const list = (params) =>
+  defHttp.get({url: Api.list, params});
+
+/**
+ * 删除单个
+ */
+export const deleteOne = (params,handleSuccess) => {
+  return defHttp.delete({url: Api.deleteOne, params}, {joinParamsToUrl: true}).then(() => {
+    handleSuccess();
+  });
+}
+/**
+ * 批量删除
+ * @param params
+ */
+export const batchDelete = (params, handleSuccess) => {
+  createConfirm({
+    iconType: 'warning',
+    title: '确认删除',
+    content: '是否删除选中数据',
+    okText: '确认',
+    cancelText: '取消',
+    onOk: () => {
+      return defHttp.delete({url: Api.deleteBatch, data: params}, {joinParamsToUrl: true}).then(() => {
+        handleSuccess();
+      });
+    }
+  });
+}
+/**
+ * 保存或者更新
+ * @param params
+ */
+export const saveOrUpdate = (params, isUpdate) => {
+  let url = isUpdate ? Api.edit : Api.save;
+  return defHttp.post({url: url, params});
+}
