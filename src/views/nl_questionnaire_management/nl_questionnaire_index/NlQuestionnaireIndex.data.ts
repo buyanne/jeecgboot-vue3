1f757b2da8472615592861f7abd9bee0bd6c35d0
buyanne <88044787+buyanne@users.noreply.github.com>
2024-04-20 10:40:43 +0800

    update

--- src/views/nl_questionnaire_management/nl_questionnaire_index/NlQuestionnaireIndex.data.ts
+++ src/views/nl_questionnaire_management/nl_questionnaire_index/NlQuestionnaireIndex.data.ts
@@ -1,113 +1,122 @@
-import {BasicColumn} from '/@/components/Table';
-import {FormSchema} from '/@/components/Table';
-import { rules} from '/@/utils/helper/validator';
-import { render } from '/@/utils/common/renderUtils';
+
+import { BasicColumn } from "/@/components/Table";
+import { FormSchema } from "/@/components/Table";
+import { rules } from "/@/utils/helper/validator";
+import { render } from "/@/utils/common/renderUtils";
+import {
+  getIndexTree
+} from "@/views/nl_questionnaire_management/nl_questionnaire_index/NlQuestionnaireIndex.api";
 //列表数据
 export const columns: BasicColumn[] = [
-   {
-    title: '上级节点',
-    align:"center",
-    dataIndex: 'parentId'
-   },
-   {
-    title: '指标名称',
-    align:"center",
-    dataIndex: 'indexName'
-   },
-   {
-    title: '指标权重',
-    align:"center",
-    dataIndex: 'indexWeight'
-   },
-   {
-    title: '指标顺序',
-    align:"center",
-    dataIndex: 'orderNum'
-   },
-   {
-    title: '适用阶段',
-    align:"center",
-    dataIndex: 'stageType'
-   },
+  {
+    title: "上级节点",
+    align: "left",
+    dataIndex: "parentId"
+  },
+  {
+    title: "指标名称",
+    align: "left",
+    dataIndex: "indexName"
+  },
+  {
+    title: "指标权重",
+    align: "center",
+    dataIndex: "indexWeight"
+  },
+  {
+    title: "指标顺序",
+    align: "center",
+    dataIndex: "orderNum"
+  },
+  {
+    title: "适用阶段",
+    align: "center",
+    dataIndex: "stageType",
+    customRender: ({ text }) => {
+      return render.renderDict(text, "stage_type");
+    }
+  }
 ];
 //查询数据
-export const searchFormSchema: FormSchema[] = [
-];
+export const searchFormSchema: FormSchema[] = [];
 //表单数据
 export const formSchema: FormSchema[] = [
   {
-    label: '上级节点',
-    field: 'parentId',
-    component: 'InputNumber',
-    dynamicRules: ({model,schema}) => {
-          return [
-                 { required: true, message: '请输入上级节点!'},
-          ];
-     },
+    label: "上级节点",
+    field: "parentId",
+    component: "JTreeSelect",
+    required: true,
+    componentProps: {
+      dict:'nl_questionnaire_index,index_name,id',
+      pidField:'parent_id',
+    },
+
   },
   {
-    label: '指标名称',
-    field: 'indexName',
-    component: 'Input',
-    dynamicRules: ({model,schema}) => {
-          return [
-                 { required: true, message: '请输入指标名称!'},
-          ];
-     },
+    label: "指标名称",
+    field: "indexName",
+    component: "Input",
+    dynamicRules: ({ model, schema }) => {
+      return [
+        { required: true, message: "请输入指标名称!" }
+      ];
+    }
   },
   {
-    label: '指标权重',
-    field: 'indexWeight',
-    component: 'InputNumber',
-    dynamicRules: ({model,schema}) => {
-          return [
-                 { required: true, message: '请输入指标权重!'},
-          ];
-     },
+    label: "指标权重",
+    field: "indexWeight",
+    component: "InputNumber",
+    dynamicRules: ({ model, schema }) => {
+      return [
+        { required: true, message: "请输入指标权重!" }
+      ];
+    }
   },
   {
-    label: '指标顺序',
-    field: 'orderNum',
-    component: 'InputNumber',
-    dynamicRules: ({model,schema}) => {
-          return [
-                 { required: true, message: '请输入指标顺序!'},
-          ];
-     },
+    label: "指标顺序",
+    field: "orderNum",
+    component: "InputNumber",
+    required:false,
+    dynamicRules: ({ model, schema }) => {
+      return [
+        { required: false, message: "请输入指标顺序!" }
+      ];
+    }
   },
   {
-    label: '适用阶段',
-    field: 'stageType',
-    component: 'InputNumber',
-    dynamicRules: ({model,schema}) => {
-          return [
-                 { required: true, message: '请输入适用阶段!'},
-          ];
-     },
+    label: "适用阶段",
+    field: "stageType",
+    component: "JDictSelectTag",
+    required:true,
+    componentProps: {
+      dictCode: "stage_type",
+      placeholder: "请选择适用阶段",
+      stringToNumber: true
+    }
   },
-	// TODO 主键隐藏字段，目前写死为ID
-	{
-	  label: '',
-	  field: 'id',
-	  component: 'Input',
-	  show: false
-	},
+  // TODO 主键隐藏字段，目前写死为ID
+  {
+    label: "",
+    field: "id",
+    component: "Input",
+    show: false
+  }
 ];
 
 // 高级查询数据
 export const superQuerySchema = {
-  parentId: {title: '上级节点',order: 0,view: 'number', type: 'number',},
-  indexName: {title: '指标名称',order: 1,view: 'text', type: 'string',},
-  indexWeight: {title: '指标权重',order: 2,view: 'number', type: 'number',},
-  orderNum: {title: '指标顺序',order: 3,view: 'number', type: 'number',},
-  stageType: {title: '适用阶段',order: 4,view: 'number', type: 'number',},
+  parentId: { title: "上级节点", order: 0, view: "number", type: "number" },
+  indexName: { title: "指标名称", order: 1, view: "text", type: "string" },
+  indexWeight: { title: "指标权重", order: 2, view: "number", type: "number" },
+  orderNum: { title: "指标顺序", order: 3, view: "number", type: "number" },
+  stageType: { title: "适用阶段", order: 4, view: "number", type: "number" }
 };
 
 /**
-* 流程表单调用这个方法获取formSchema
-* @param param
-*/
-export function getBpmFormSchema(_formData): FormSchema[]{
+ * 流程表单调用这个方法获取formSchema
+ * @param param
+ */
+export function getBpmFormSchema(_formData): FormSchema[] {
   // 默认和原始表单保持一致 如果流程中配置了权限数据，这里需要单独处理formSchema
   return formSchema;
 }
--- src/views/nl_questionnaire_management/nl_questionnaire_index/components/NlQuestionnaireIndexModal.vue
+++ src/views/nl_questionnaire_management/nl_questionnaire_index/components/NlQuestionnaireIndexModal.vue
@@ -1,66 +1,83 @@
+
 <template>
-  <BasicModal v-bind="$attrs" @register="registerModal" destroyOnClose :title="title" :width="800" @ok="handleSubmit">
-      <BasicForm @register="registerForm"/>
+  <BasicModal v-bind="$attrs" @register="registerModal" destroyOnClose :title="title" :width="800"
+              @ok="handleSubmit">
+    <BasicForm @register="registerForm" />
   </BasicModal>
 </template>
 
 <script lang="ts" setup>
-    import {ref, computed, unref} from 'vue';
-    import {BasicModal, useModalInner} from '/@/components/Modal';
-    import {BasicForm, useForm} from '/@/components/Form/index';
-    import {formSchema} from '../NlQuestionnaireIndex.data';
-    import {saveOrUpdate} from '../NlQuestionnaireIndex.api';
-    // Emits声明
-    const emit = defineEmits(['register','success']);
-    const isUpdate = ref(true);
-    //表单配置
-    const [registerForm, {setProps,resetFields, setFieldsValue, validate}] = useForm({
-        //labelWidth: 150,
-        schemas: formSchema,
-        showActionButtonGroup: false,
-        baseColProps: {span: 24}
+import { ref, computed, unref } from "vue";
+import { BasicModal, useModalInner } from "/@/components/Modal";
+import { BasicForm, useForm } from "/@/components/Form/index";
+import { formSchema } from "../NlQuestionnaireIndex.data";
+import { saveOrUpdate } from "../NlQuestionnaireIndex.api";
+// Emits声明
+const emit = defineEmits(["register", "success"]);
+const isUpdate = ref(true);
+const addType = ref(1);
+//表单配置
+const [registerForm, { setProps, resetFields, setFieldsValue, validate }] = useForm({
+  //labelWidth: 150,
+  schemas: formSchema,
+  showActionButtonGroup: false,
+  baseColProps: { span: 24 }
+});
+//表单赋值
+const [registerModal, { setModalProps, closeModal }] = useModalInner(async (data) => {
+  //重置表单
+  await resetFields();
+  setModalProps({
+    confirmLoading: false,
+    showCancelBtn: !!data?.showFooter,
+    showOkBtn: !!data?.showFooter
+  });
+  isUpdate.value = !!data?.isUpdate;
+  addType.value = data?.record?.addType;
+  if(unref(addType)){
+    await setFieldsValue({
+      ...data.record
     });
+  }
+  // console.log(isUpdate,addType)
+  if (unref(isUpdate)) {
     //表单赋值
-    const [registerModal, {setModalProps, closeModal}] = useModalInner(async (data) => {
-        //重置表单
-        await resetFields();
-        setModalProps({confirmLoading: false,showCancelBtn:!!data?.showFooter,showOkBtn:!!data?.showFooter});
-        isUpdate.value = !!data?.isUpdate;
-        if (unref(isUpdate)) {
-            //表单赋值
-            await setFieldsValue({
-                ...data.record,
-            });
-        }
-        // 隐藏底部时禁用整个表单
-       setProps({ disabled: !data?.showFooter })
+    await setFieldsValue({
+      ...data.record
     });
-    //设置标题
-    const title = computed(() => (!unref(isUpdate) ? '新增' : '编辑'));
-    //表单提交事件
-    async function handleSubmit(v) {
-        try {
-            let values = await validate();
-            setModalProps({confirmLoading: true});
-            //提交表单
-            await saveOrUpdate(values, isUpdate.value);
-            //关闭弹窗
-            closeModal();
-            //刷新列表
-            emit('success');
-        } finally {
-            setModalProps({confirmLoading: false});
-        }
-    }
+  }
+  // 隐藏底部时禁用整个表单
+  setProps({ disabled: !data?.showFooter });
+});
+//设置标题
+const title = computed(() => (
+
+  !unref(isUpdate) ? !unref(addType) ?"新增":'添加下级' : "编辑"));
+
+//表单提交事件
+async function handleSubmit(v) {
+  try {
+    let values = await validate();
+    setModalProps({ confirmLoading: true });
+    //提交表单
+    await saveOrUpdate(values, isUpdate.value);
+    //关闭弹窗
+    closeModal();
+    //刷新列表
+    emit("success");
+  } finally {
+    setModalProps({ confirmLoading: false });
+  }
+}
 </script>
 
 <style lang="less" scoped>
-	/** 时间和数字输入框样式 */
-  :deep(.ant-input-number){
-		width: 100%
-	}
+/** 时间和数字输入框样式 */
+:deep(.ant-input-number) {
+  width: 100%
+}
 
-	:deep(.ant-calendar-picker){
-		width: 100%
-	}
+:deep(.ant-calendar-picker) {
+  width: 100%
+}
 </style>
