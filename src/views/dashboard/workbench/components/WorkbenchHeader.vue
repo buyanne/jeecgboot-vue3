56c000d4c4fb4d3adb8fa1a6c8354e35d4aed971
buyanne <88044787+buyanne@users.noreply.github.com>
2024-05-12 03:18:26 +0800

    简单的主页

--- src/views/dashboard/workbench/components/WorkbenchHeader.vue
+++ src/views/dashboard/workbench/components/WorkbenchHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="lg:flex">
-    <Avatar :src="userinfo.avatar || headerImg" :size="72" class="!mx-auto !block" />
+    <Avatar :src="avatar || headerImg" :size="72" class="!mx-auto !block" />
     <div class="md:ml-6 flex flex-col justify-center md:mt-0 mt-2">
       <h1 class="md:text-lg text-md">早安, {{ userinfo.realname }}, 开始您一天的工作吧！</h1>
       <span class="text-secondary"> 今日晴，20℃ - 32℃！ </span>
@@ -27,7 +27,11 @@
   import { Avatar } from 'ant-design-vue';
   import { useUserStore } from '/@/store/modules/user';
   import headerImg from '/@/assets/images/header.jpg';
+  import { getFileAccessHttpUrl } from "@/utils/common/compUtils";
 
   const userStore = useUserStore();
   const userinfo = computed(() => userStore.getUserInfo);
+  const avatar = computed(() => {
+    return getFileAccessHttpUrl(userinfo.value.avatar) || headerImg;
+  });
 </script>
--- src/views/nl_personnel_capability_portrait/Portrait.vue
+++ src/views/nl_personnel_capability_portrait/Portrait.vue
@@ -1,12 +1,43 @@
 <template>
-  <div>
-    nihao
-  </div>
+  <PageWrapper>
+    <template #headerContent>
+      <PortraitHeader />
+    </template>
+    <div class="lg:flex">
+      <div class="lg:w-7/10 w-full !mr-4 enter-y">
+        <ProjectCard :loading="loading" class="enter-y" />
+        <DynamicInfo :loading="loading" class="!my-4 enter-y" />
+      </div>
+      <div class="lg:w-3/10 w-full enter-y">
+        <QuickNav :loading="loading" class="enter-y" />
+
+        <Card class="!my-4 enter-y" :loading="loading">
+          <img class="xl:h-50 h-30 mx-auto" src="../../../assets/svg/illustration.svg" />
+        </Card>
+
+        <SaleRadar :loading="loading" class="enter-y" />
+      </div>
+    </div>
+  </PageWrapper>
+
+
 </template>
-<script setup lang="ts">
+<script lang="ts" setup>
+
+import PageWrapper from "@/components/Page/src/PageWrapper.vue";
+import PortraitHeader from "@/views/nl_personnel_capability_portrait/component/PortraitHeader.vue";
+import SaleRadar from "@/views/dashboard/workbench/components/SaleRadar.vue";
+import DynamicInfo from "@/views/dashboard/workbench/components/DynamicInfo.vue";
+import { Card } from "ant-design-vue";
+import QuickNav from "@/views/dashboard/workbench/components/QuickNav.vue";
+import ProjectCard from "@/views/dashboard/workbench/components/ProjectCard.vue";
+import { ref } from "vue";
+
+
+const loading = ref(true);
+
+setTimeout(() => {
+  loading.value = false;
+}, 500);
 
 </script>
-
-<style scoped lang="less">
-
-</style>
